78d9573a1c376ddb79c09cc8d6c7e2265681fa02
Vinipet <79338898+vinipet@users.noreply.github.com>
2024-10-03 18:16:17 -0300

    setup

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import telebot
 Chave_api = "7352283955:AAE2KNW-GlifnPKwy82F0iytRP2uDFPlJSA"
 
 bot = telebot.TeleBot(Chave_api)
-
-
-def StandartMensage(msg)
-   bot.re
+def verify()
+@bot.message_handler(func=verify)
+def StandartMensage(msg):
+   bot.reply_to(msg, "Mensagem padrão!")
 
 bot.polling()
