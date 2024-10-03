b9e9a7c5481c62700e74bf6963edd4c019f18374
Vinipet <79338898+vinipet@users.noreply.github.com>
2024-10-03 19:19:32 -0300

    py

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import telebot
 Chave_api = "7352283955:AAE2KNW-GlifnPKwy82F0iytRP2uDFPlJSA"
 
 bot = telebot.TeleBot(Chave_api)
-def verify()
+def verify(msg):
+   return True
 @bot.message_handler(func=verify)
 def StandartMensage(msg):
    bot.reply_to(msg, "Mensagem padrão!")
