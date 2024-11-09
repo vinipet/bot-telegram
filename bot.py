420fb21298b8136f84aadb9e873c4be2f5040cbe
Vinipet <79338898+vinipet@users.noreply.github.com>
2024-11-08 21:32:55 -0300

    help command add

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ bot = telebot.TeleBot(Chave_api)
 
 bot.delete_my_commands()
 
+
 def add_command(commandName, commandDescription):
     currentCommands = bot.get_my_commands()
     newCommand = types.BotCommand(commandName, commandDescription)
@@ -15,17 +16,25 @@ def add_command(commandName, commandDescription):
 
 # @bot.message_handler(func = lambda msg: msg.text == 'me add')
 
-add_command('start', 'inicia o bot')
+add_command('start', ' 🚀 inicia o bot')
 @bot.message_handler(commands=['start'])
 def startCommand(msg):
-   bot.reply_to(msg, " Olá! 👋 Bem-vindo ao [nome do seu bot]! \n \n  Estou aqui para ajudar você com [objetivo principal do bot]. Aqui estão algumas coisas que você pode fazer comigo: \n \n 📄 /help - Veja uma lista completa dos meus comandos \n ℹ️ /info - Saiba mais sobre o que eu posso fazer \n 🆘 /support - Fale com o suporte para mais ajuda \n \n Se precisar de algo específico, é só digitar o comando ou enviar uma mensagem. Vamos começar! 🚀")
+   bot.reply_to(msg, " Olá! 👋 Bem-vindo! \n \n  Estou aqui para ajudar você a entrar no canal privado [sla que nome c vai dar pedro]. Aqui estão algumas coisas que você pode fazer comigo: \n \n 📄 /help - Veja uma lista completa dos meus comandos \n ℹ️ /info - Saiba mais sobre o que eu posso fazer \n 🆘 /support - Fale com o suporte para mais ajuda \n \n Se precisar de algo específico, é só digitar o comando ou enviar uma mensagem. Vamos começar! 🚀")
 
-add_command('myinfo','exibe as informações do usuario')
+add_command('help', ' 🔍 Exibe lista completa de Comandos')
+@bot.message_handler(commands=['help'])
+def helpcommand(msg):
+   bot.reply_to(msg, 'Aqui está tudo o que você pode fazer comigo!')
+   for command in bot.get_my_commands():
+      bot.send_message(msg.chat.id, '/{} -   {} '.format(command.command ,command.description))
+   bot.send_message(msg.chat.id, 'Para executar qualquer um, basta digitar o nome do comando ou clicar nele. Qualquer dúvida, estou aqui para ajudar! 😃') 
+
+add_command('myinfo',' 📄 exibe as informações do usuario')
 @bot.message_handler(commands=['myinfo'])
 def infosCommand(msg):
    bot.reply_to(msg, "claro, aqui estão algumas info suas \n seu id {} \n seu primeiro nome {} \n".format(msg.json['from']['id'], msg.json['from']['first_name']))
 
-add_command('logs', 'exibe alguns logs da programação')
+add_command('logs', '\U0001FAB5 exibe alguns logs da programação')
 @bot.message_handler(commands=['logs'])
 def sendLogsCommand(msg):
    bot.reply_to(msg, ('os dados da msg são >>>>>>>> ' + json.dumps(msg.json, indent=4)))
@@ -38,9 +47,8 @@ def verify(msg):
 @bot.message_handler(func=verify)
 def StandartMensage(msg):
    commands = bot.get_my_commands()
-   bot.send_message(msg.chat.id, 'Não entendi oque você quis dizer, aqui estão alguns comandos que podem te ajudar \n \n 📄 /help - Veja uma lista completa dos meus comandos \n 🚀 /start - comece pelo inicio :)' )
+   bot.send_message(msg.chat.id, 'Não entendi oque você quis dizer, aqui estão alguns comandos que podem te ajudar \n \n 📄 /help - Veja uma lista completa dos meus comandos \n 🚀 /start - comece pelo inicio :) ' )
    print('id de usuario >>>', msg.from_user.id)
    print('nome de usuario >>>>', msg.from_user.first_name)
-
 
 bot.infinity_polling()
