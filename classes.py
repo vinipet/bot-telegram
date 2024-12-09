b2c744ff98fd84f2d64d1fe4fffcf2e680d71c42
Vinipet <79338898+vinipet@users.noreply.github.com>
2024-12-09 12:07:10 -0300

    aded some utilities for push

--- classes.py
+++ classes.py
@@ -1,17 +1,17 @@
 class Usertemp:
     def __init__(self, userId, infos):
-      self.id = userId,
-      self.steps = infos
-      self.email = None
-      self.firstName = None
-      self.lastName = None
-      self.identification = None
-      # {"type": None,"number": None}
-      self.address = {
+        self.id = (userId,)
+        self.steps = infos
+        self.email = None
+        self.firstName = None
+        self.lastName = None
+        self.identification = None
+        # {"type": None,"number": None}
+        self.address = {
             "zip_code": None,
             "street_name": None,
             "street_number": None,
             "neighborhood": None,
             "city": None,
-            "federal_unit": None
-      }
+            "federal_unit": None,
+        }
--- fetch.py
+++ fetch.py
@@ -1,30 +1,28 @@
-import mercadopago
-from dotenv import load_dotenv
+import json
 import os
 import random
-import json
 
+import mercadopago
+from dotenv import load_dotenv
 
 load_dotenv()
-sdk = mercadopago.SDK(os.getenv('accessToken'))
+sdk = mercadopago.SDK(os.getenv("accessToken"))
 
 generated_keys = set()
 
+
 def generate_unique_key():
     while True:
-        key = ''.join(random.choices('0123456789', k=3))
+        key = "".join(random.choices("0123456789", k=3))
         if key not in generated_keys:
             generated_keys.add(key)
             return key
 
 
-
 def fetch(user):
-    url = 'https://api.mercadopago.com/v1/payments'
+    url = "https://api.mercadopago.com/v1/payments"
     request_options = mercadopago.config.RequestOptions()
-    request_options.custom_headers = {
-        'x-idempotency-key': str(user.id)
-    }
+    request_options.custom_headers = {"x-idempotency-key": str(user.id)}
 
     payment_data = {
         "transaction_amount": 100,
@@ -35,10 +33,7 @@ def fetch(user):
             "email": user.email,
             "first_name": user.firstName,
             "last_name": user.lastName,
-            "identification": {
-                "type": "CPF",
-                "number": user.identification
-            },
+            "identification": {"type": "CPF", "number": user.identification},
             # "address": {
             #     "zip_code": "06233-200",
             #     "street_name": "Av. das Nações Unidas",
@@ -47,29 +42,38 @@ def fetch(user):
             #     "city": "Osasco",
             #     "federal_unit": "SP"
             # }
-        }
+        },
     }
-
 
     payment = sdk.payment().create(payment_data, request_options)
 
     if payment["status"] == 200:
         payment_response = payment["response"]  # Dados do pagamento criado
-        print("Pagamento criado com sucesso!", json.dumps(payment_response, indent=4),  '\n \n \n')
-        response = { 
-            'QRCode' : payment_response['point_of_interaction']['transaction_data']['qr_code'] ,
-            'QRCode64' : payment_response['point_of_interaction']['transaction_data']['qr_code_base64'] ,
-            'link' :  payment_response['point_of_interaction']['transaction_data']['ticket_url'],
-            'status' : 'aproved'
+        print(
+            "Pagamento criado com sucesso!",
+            json.dumps(payment_response, indent=4),
+            "\n \n \n",
+        )
+        response = {
+            "QRCode": payment_response["point_of_interaction"]["transaction_data"][
+                "qr_code"
+            ],
+            "QRCode64": payment_response["point_of_interaction"]["transaction_data"][
+                "qr_code_base64"
+            ],
+            "link": payment_response["point_of_interaction"]["transaction_data"][
+                "ticket_url"
+            ],
+            "status": "aproved",
         }
         return response
 
     else:
         print(f"Erro ao criar pagamento: {payment['status']} -> {payment['response']}")
         response = {
-            'status' : 'denied',
-            'msg' : payment['response'].message  # Define um valor padrão para response em caso de erro
+            "status": "denied",
+            "msg": payment[
+                "response"
+            ].message,  # Define um valor padrão para response em caso de erro
         }
         return response
-
-    
