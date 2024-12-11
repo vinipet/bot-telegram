e481ac051db78446b65c1b2e33bcdbad6f52255b
Vinipet <79338898+vinipet@users.noreply.github.com>
2024-12-11 07:27:48 -0300

    identação de pre commit

--- tests/test_method/tryRegisterUser_test.py
+++ tests/test_method/tryRegisterUser_test.py
@@ -1,7 +1,9 @@
+from unittest.mock import *
+
 import pytest
+
 import bot
 import classes
-from unittest.mock import *
 
 
 def compare_objects(obj1, obj2):
@@ -22,31 +24,37 @@ def compare_objects(obj1, obj2):
 
 @pytest.fixture
 def mockBank():
-   with patch("bot.bancoDdados" , {
-      "1": classes.Usertest(), 
-      "2": {"name": "Bob"} } ) , \
-        patch("bot.userData", {
-      "3": {"name": "Charlie"}, 
-      "4":classes.Usertest(),
-      "5":classes.User(123, infos= "1"),
-      "6":classes.User(9090,["quero boquete parafuso", "name", "email"])}):
-      
+    with patch(
+        "bot.bancoDdados", {"1": classes.Usertest(), "2": {"name": "Bob"}}
+    ), patch(
+        "bot.userData",
+        {
+            "3": {"name": "Charlie"},
+            "4": classes.Usertest(),
+            "5": classes.User(123, infos="1"),
+            "6": classes.User(9090, ["quero boquete parafuso", "name", "email"]),
+        },
+    ):
         yield bot
- 
+
+
 def test_try_register_dict_user(mockBank):
-   with pytest.raises(TypeError):
-      bot.tryRegisterUser("3")
+    with pytest.raises(TypeError):
+        bot.tryRegisterUser("3")
+
 
 def test_try_register_object_user(mockBank):
-   preObj = bot.userData["4"]
-   bot.tryRegisterUser("4")
-   assert compare_objects(bot.bancoDdados["4"], preObj) == True
+    preObj = bot.userData["4"]
+    bot.tryRegisterUser("4")
+    assert compare_objects(bot.bancoDdados["4"], preObj) == True
+
 
 def test_try_register_user_without_steps(mockBank):
-     with pytest.raises(TypeError):
-      bot.tryRegisterUser("5")
+    with pytest.raises(TypeError):
+        bot.tryRegisterUser("5")
+
 
 def test_try_register_user_with_somuch_steps(mockBank):
-   bot.tryRegisterUser("6")
-   assert "6" in bot.userData 
-   assert "6" not in bot.bancoDdados 
+    bot.tryRegisterUser("6")
+    assert "6" in bot.userData
+    assert "6" not in bot.bancoDdados
